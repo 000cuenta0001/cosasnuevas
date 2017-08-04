11ed5ed7a1b4fc88de62e810fe632af7e9918e16
puesyo <000cuenta0001@gmail.com>
2017-08-05 00:12:14 +0200

    j

--- addons/plugin.video.mitvspain/channels/playpornx.py
+++ addons/plugin.video.mitvspain/channels/playpornx.py
@@ -4,6 +4,7 @@
 # ------------------------------------------------------------
 
 import re
+import sys
 
 from core import httptools
 from core import logger
@@ -13,29 +14,23 @@ from core.item import Item
 host ="http://www.playpornx.net/list-movies/"
 
 def mainlist (item):
-    itemlist = []
-    itemlist.append(Item(channel=item.channel, title="Todas", action="lista",
-                         thumbnail='https://s18.postimg.org/fwvaeo6qh/todas.png',
-                         fanart='https://s18.postimg.org/fwvaeo6qh/todas.png',
-                         url ='https://www.playpornx.net/category/porn-movies/?filter=date'))
-    itemlist.append(Item(channel=item.channel, title="Buscar", action="search", url='http://www.playpornx.net/?s=',
-                         thumbnail='https://s30.postimg.org/pei7txpa9/buscar.png',
-                         fanart='https://s30.postimg.org/pei7txpa9/buscar.png'))
+    itemlist =[]
+    itemlist.append( Item(channel=item.channel, title="Todas", action="lista", thumbnail='https://s18.postimg.org/fwvaeo6qh/todas.png', fanart='https://s18.postimg.org/fwvaeo6qh/todas.png'))
+    itemlist.append( Item(channel=item.channel, title="Buscar", action="search", url='http://www.playpornx.net/?s=', thumbnail='https://s30.postimg.org/pei7txpa9/buscar.png', fanart='https://s30.postimg.org/pei7txpa9/buscar.png'))
 
     return itemlist
-	
+
 def lista (item):
     logger.info()
     
     itemlist = []
     if item.url =='': item.url=host
     data = httptools.downloadpage(item.url).data
-    data = re.sub(r'"|\n|\r|\t|&nbsp;|<br>|\s{2,}', "", data)
-    logger.debug(data)
-    patron = 'role=article><a href=(.*?) rel=bookmark title=(.*?)>.*?src=(.*?) class'
-    matches = re.compile(patron, re.DOTALL).findall(data)
+    patron = '<a class="clip-link" title="([^"]+)"  href="([^"]+)">\s*<span class="clip">\s*<img  alt=".*?" width="190" height="266" src="([^"]+)" data-qazy="true" \/><span class="vertical-align"><\/span>\s*<\/span>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
 
-    for scrapedurl, scrapedtitle, scrapedthumbnail in matches:
+    for scrapedtitle, scrapedurl, scrapedthumbnail in matches:
+
         url = scrapedurl
         thumbnail = scrapedthumbnail
         title = scrapedtitle
@@ -48,10 +43,8 @@ def lista (item):
         actual_page_url = item.url
         next_page = scrapertools.find_single_match(data,'rel="next" href="([^"]+)"')
         if next_page !='':
-           itemlist.append(Item(channel = item.channel, action = "lista", title = 'Siguiente >>>', url = next_page, 
-		   thumbnail='https://s16.postimg.org/9okdu7hhx/siguiente.png',extra=item.extra))
-                                
-	return itemlist
+           itemlist.append(Item(channel = item.channel, action = "lista", title = 'Siguiente >>>', url = next_page, thumbnail='https://s16.postimg.org/9okdu7hhx/siguiente.png',extra=item.extra))
+    return itemlist
 
 def search (item,texto):
     logger.info()
